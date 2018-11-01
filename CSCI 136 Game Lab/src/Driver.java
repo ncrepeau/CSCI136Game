e8ee3c5890e828903ff242bd35b43d15fa6f1e4c
ncrep <ncrep@10.17.139.198>
2018-11-01 15:26:03 -0600

    Driver and item push

---
+++ CSCI 136 Game Lab/src/Driver.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Driver extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		//Player image and position
+		Player myPlayer = new Player();
+		Image player = new Image(myPlayer.getPlayerImg());
+		ImageView viewPlayer = new ImageView(player);
+		int playerX = myPlayer.setPlayerXPosition();
+		int playerY = myPlayer.setPlayerYPosition();
+		viewPlayer.setLayoutX(playerX);
+		viewPlayer.setLayoutY(playerY);
+		
+		//Enemy image and position
+		Enemy myEnemy = new Enemy();
+		Image enemy = new Image(myEnemy.getEnemyImg());
+		ImageView viewEnemy = new ImageView(enemy);
+		int enemyX = myEnemy.setEnemyXPosition();
+		int enemyY = myEnemy.setEnemyYPosition();
+		viewEnemy.setLayoutX(enemyX);
+		viewEnemy.setLayoutY(enemyY);
+		viewEnemy.setFitHeight(190);
+		viewEnemy.setFitWidth(150);
+		
+		//Items and their positions
+		item myItem = new item();
+		ArrayList<String> itemArray = new ArrayList<String>();
+		for(int i = 0; i < 5; i++) {
+			itemArray = item.getImagePath();
+		}
+		for(int i = 0; i < itemArray.size(); i++) {
+			myItem.setPosition(itemArray.get(0));
+		}		
+		
+		//Rest Button
+		Button reset = new Button("Restart");
+		reset.setLayoutX(740);
+		reset.setLayoutY(15);
+        
+        //Button Action
+        reset.setOnAction(e -> {
+        	viewPlayer.setLayoutX(myPlayer.setPlayerXPosition());
+        	viewPlayer.setLayoutY(myPlayer.setPlayerYPosition());
+        	
+        	viewEnemy.setLayoutX(myEnemy.setEnemyXPosition());
+        	viewEnemy.setLayoutY(myEnemy.setEnemyYPosition());
+        	
+        	//viewItem1.setLayoutX(myItems.setItemXPosition());
+        	//viewItem1.setLayoutY(myItems.setItemYPosition());
+        });
+		
+		Group myGroup = new Group(viewPlayer, viewEnemy, reset); //the group of objects that will be added to the window
+		Scene background = new Scene(myGroup, 800, 600, Color.WHITE);
+		
+		primaryStage.setTitle("Game"); //title of window in the window's bar
+		primaryStage.setScene(background); //implements the "scene" (objects/background)
+		primaryStage.show(); //shows the window (would probably not appear w/o it)
+	}
+}
--- CSCI 136 Game Lab/src/item.java
+++ CSCI 136 Game Lab/src/item.java
@@ -1,21 +1,40 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
-public class item {
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class item  {
 	private String imagePath;
 	private int position;
 	private double itemWorth;
 
-	public item(String imagePath, int position, double itemWorth) {
-		this.imagePath = imagePath;
-		this.position = position;
-		this.itemWorth = itemWorth;
+	public item() {
 	}
 
-	public String getImagePath() {
-		return imagePath;
+	public static ArrayList<String> getImagePath() throws FileNotFoundException {
+		ArrayList<String> itemArray = new ArrayList<String>();
+		File myFile = new File("./src/ItemFile.txt");
+		Scanner myFileScanner = new Scanner(myFile);
+		while(myFileScanner.hasNextLine()) {
+			String line = myFileScanner.nextLine();
+			itemArray.add(line);
+		}
+		return itemArray;
 	}
 
-	public void setPosition(int position) {
-		this.position = position;
+	public void setPosition(String imagePath ) {
+		Random rand = new Random();
+		int itemX = rand.nextInt(1150)+1;
+		int itemY = rand.nextInt(750)+1;
+		Image myImage = new Image(imagePath);
+		ImageView myImageView = new ImageView(myImage);
+		myImageView.setX(itemX);
+		myImageView.setY(itemY);
+		
 	}
 
 	public void setItemWorth(double itemWorth) {
