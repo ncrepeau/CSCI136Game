3296aaf05aae94d14f0e794377d1faf1bb352627
ncrep <ncrep@10.17.139.198>
2018-11-02 11:38:59 -0600

    Abstract of movement class, haven't implemented in the scene so they can't move yet, but this is the framework.

--- CSCI 136 Game Lab/src/movement.java
+++ CSCI 136 Game Lab/src/movement.java
@@ -1,22 +1,46 @@
+import java.util.Random;
+
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 
 public class movement{
+	Random rand = new Random();
 	public movement(){
 	
 		
 	}
 	
 	public void playerMovement() {
-		
+	myScene.setOnKeyPressed(this::keyPress);
+	
+	}
+	public void keyPress(KeyEvent event) {
+	KeyCode myCode = event.getCode();
+	if(myCode == KeyCode.A) {
+		int x =- 10;
+	}
+	else if(myCode== KeyCode.D) {
+		int x =+ 10;
+	}
+	else if(myCode == KeyCode.S) {
+		int y =+ 10;
+	}
+	else if(myCode == KeyCode.W) {
+		int y =- 10;
+	}
+	int n = rand.nextInt(10)+1;
+	viewPlayer.setLayoutX(x);
+	viewPlayer.setLayoutY(y);
+	viewEnemy.setLayoutX(x-n);
+	viewEnemy.setLayoutY(y-n);
 	}
 	
-	public void enemyMovement() {
-		
-	}
 	
-	public String keyListener() {
-		return "b";
+
+	
+	
 		
-	}
+	
 }
